d11a4e053bc7c87e49017c55e4d4d8b27a5ebfb4
liam <39243060+devldm@users.noreply.github.com>
2023-09-25 18:51:35 +0900

    style changes

--- src/components/GridWithTitle.tsx
+++ src/components/GridWithTitle.tsx
@@ -10,17 +10,20 @@ export default function GridWithTitle({
   return (
     <div className="w-full p-2">
       <h1 className="mb-4 text-2xl">{title}</h1>
-      <div className="flex flex-col gap-3 md:flex md:flex-wrap md:gap-4">
+      <div className="flex flex-col gap-3 md:flex md:flex-row md:flex-wrap md:gap-4">
         {data
           ? data.map((solution) => {
               return (
                 <div
                   key={solution.id}
-                  className="rounded-lg border-2 border-gray-300 p-5 md:min-w-[300px] md:max-w-[350px]"
+                  className="flex flex-col justify-between gap-3 rounded-lg border-2 border-gray-300 p-5 md:w-[calc(830px_/_2)]"
                 >
-                  <p>{solution.grammar}</p>
-                  <p>{solution.solution}</p>
-                  <p>{solution.solvedAt?.toLocaleDateString()}</p>
+                  <p className="max-w-max rounded-lg border-2 border-slate-500 bg-slate-800 p-2">
+                    {solution.grammar}
+                  </p>
+
+                  <p className="text-lg font-bold">{solution.solution}</p>
+                  <p className="">{solution.solvedAt?.toLocaleDateString()}</p>
                 </div>
               );
             })
