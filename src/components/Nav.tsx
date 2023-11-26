d0b9fe20a1c882d6c06dc8306be721079aeb820c
liam <39243060+devldm@users.noreply.github.com>
2023-11-26 10:56:16 +0900

    update nav to handle active pages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,21 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type link = {
+  linkText: string;
+  href: string;
+};
+
+const baseLinks: link[] = [{ linkText: "Home", href: "/" }];
+
+const signedInLinks: link[] = [
+  ...baseLinks,
+  { linkText: "Profile", href: "/profile" },
+];
 
 export default function Nav({ isSignedIn }: { isSignedIn: boolean }) {
+  const pathname = usePathname();
+  const links = isSignedIn ? signedInLinks : baseLinks;
   return (
     <nav className="border-gray-200 bg-white dark:bg-gray-900">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
@@ -39,25 +54,21 @@ export default function Nav({ isSignedIn }: { isSignedIn: boolean }) {
         </button>
         <div className="hidden w-full md:block md:w-auto" id="menu">
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
-            <li>
-              <Link
-                href="/"
-                className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            {isSignedIn && (
-              <li>
+            {links.map((link) => (
+              <li key={link.href} className="flex">
                 <Link
-                  href="/profile"
-                  className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                  href={link.href}
+                  passHref
+                  className={
+                    pathname === link.href
+                      ? " text-blue-500"
+                      : "text-gray-900 dark:text-gray-100"
+                  }
                 >
-                  Profile
+                  {link.linkText}
                 </Link>
               </li>
-            )}
+            ))}
           </ul>
         </div>
       </div>
